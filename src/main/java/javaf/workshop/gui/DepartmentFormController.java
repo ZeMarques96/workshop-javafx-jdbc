7d66a2de03517f73a8c42e86762c3b2e961c9fed
José Francisco Marques Neto <netomarques96@gmail.com>
2024-07-10 16:56:28 -0300

    DepartmentForm Controller

---
+++ src/main/java/javaf/workshop/gui/DepartmentFormController.java
@@ -0,0 +1,48 @@
+package javaf.workshop.gui;
+
+import javaf.workshop.gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSavenAction(){
+        System.out.println("Save");
+    }
+
+    @FXML
+    public void onBtCancelAction(){
+        System.out.println("Cancel");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+}
